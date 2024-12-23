d4d7d64ad2fba95588ff04b2bdd09d3e4ff8d06a
Aroldo Souza-Leite <asouzaleite@gmx.de>
2024-12-23 01:01:08 +0100

    TODO: cleaning up.

--- youtube_download_mp3.py
+++ youtube_download_mp3.py
@@ -4,27 +4,34 @@ import tkinter as tk
 from tkinter import ttk
 import subprocess, shutil, shlex, sys, os
 import pyperclip
+from pathlib import Path
+
+command_line_fmt = 'xterm -hold -132 -bg white -fg black -fa Monospace -fs 14 -geometry 150x30  -e {executable} --config-locations  {config_dir} -- {url}'
+url_default="https://www.youtube.com/watch?v=S-rB0pHI9fU"
+executable = "yt-dlp"
+config_subdir = "mp3.d"
+
+HOME_path = Path(os.environ.get("HOME"))
+XDG_CONFIG_HOME_path  = HOME_path/".config"
+config_dir_path = XDG_CONFIG_HOME_path/executable/config_subdir
+subprocess_env = dict(
+    XDG_CONFIG_HOME = str(XDG_CONFIG_HOME_path),
+    config_dir = str(config_dir_path))
+os.environ.update(subprocess_env)
 
 root = tk.Tk()
-
-url_default="https://www.youtube.com/watch?v=S-rB0pHI9fU"
-url_string = tk.StringVar(root)
-url_string.set(url_default)
-print("Default url:", url_string.get())
-
-executable = "yt-dlp"
-config_dir = os.environ.get("config_dir")
-command_line = f'xterm -hold -132 -bg white -fg black -fa Monospace -fs 14 -geometry 150x30  -e {executable} --config-locations  {config_dir}'
-print(command_line, file = sys.stderr)
-arg_list = shlex.split(command_line)
-print(arg_list, file = sys.stderr)
-
 root.title("Herrquesada YouTube App")
 root.geometry('1200x100+150+150')
+url_tkstring = tk.StringVar(root)
+url_tkstring.set(url_default)
+# ## <debug>
+print("Default url:", url_tkstring.get())
+# ##</debug>
+
 
 prompt = ttk.Label(root, text = 'Type the video url here:')
-inputBox = ttk.Entry(root, textvariable = url_string, width = 140)
-url_default_length = len(url_string.get())
+inputBox = ttk.Entry(root, textvariable = url_tkstring, width = 140)
+url_default_length = len(url_tkstring.get())
 inputBox.selection_range(0,url_default_length)
 okButton = ttk.Button(text="Download")
 
@@ -36,7 +43,11 @@ inputBox.bind("<Button-3>", insert_url)
     
 def download(event):
     url = inputBox.get()
-    arg_list.append(url)
+    command_line = command_line_fmt.format(config_dir = os.environ.get("config_dir"),
+                                           executable = executable,
+                                           url = url)
+    arg_list = shlex.split(command_line)
+    print(arg_list)
     proc = subprocess.run(arg_list,
                           stdout = subprocess.PIPE,
                           stderr = subprocess.PIPE)
