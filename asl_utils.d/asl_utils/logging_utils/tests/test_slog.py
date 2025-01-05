f6ed1a3f4db908a1f7b32c35b64a96163b37c499
Aroldo Souza-Leite <asouzaleite@gmx.de>
2025-01-05 17:43:32 +0100

    Further test system development. test_slog_in_garbage working well.

--- asl_utils.d/asl_utils/logging_utils/tests/test_slog.py
+++ asl_utils.d/asl_utils/logging_utils/tests/test_slog.py
@@ -3,15 +3,31 @@ from .. import make_slog
 import logging
 __level__ = logging.DEBUG 
 _slog = make_slog(level = __level__)
+
 def rubbish():
-    _slog.info("Guten Tag!")
-    pass
+    x = 666
+    _slog.info(f"Guten Tag {x}!")
+    return x
+
+def garbage():
+    x = "Beelzebub"
+    _slog.info(f"Guten Tag {x}!")
+    return x
 
 def test_slog_in_rubbish():
     """
     Tests if a simple log object
-    works in the rubbsh function
+    works in the rubbish function
     namespace.
     """
-    rubbish()
+    x = rubbish()
+    assert x == 666 
 
+def test_slog_in_garbage():
+    """
+    Tests if a simple log object
+    works in the garbage function
+    namespace.
+    """
+    x = garbage()
+    assert x == 666, "Beelzebub or 666?" 
