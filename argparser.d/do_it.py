5a61821f93b7e54e0921afa0b6077a81faf83bd4
Aroldo Souza-Leite <asouzaleite@gmx.de>
2025-01-25 14:36:53 +0100

    Logging.

--- argparser.d/do_it.py
+++ argparser.d/do_it.py
@@ -2,16 +2,31 @@
 """
 Usage examples of 'argparse'.
 """
-import sys
+import sys, logging, pathlib
 import argparse
-from pathlib import Path
-_this_script_path = Path(sys.argv[0]).resolve()
+__level__ = logging.DEBUG
+logging.basicConfig()
+_slog = logging.getLogger()
+_slog.setLevel(__level__)
+_script_path = pathlib.Path(sys.argv[0]).resolve()
+_script_name = _script_path.with_suffix("").name 
+msg = f"\nSCRIPT: {_script_name}\n"
+_slog.debug(msg)
 
 #
 parser = argparse.ArgumentParser(description = """
     This script reads each line of the batch file and
     interprets each line  as the name of one package to be installed.
     """)
+parser.add_argument(
+    "-c",
+    "--command",
+    metavar = "COMMAND",
+    type = str,
+    dest = "command",
+    default = "echo",
+    help = "One argument per line.",
+)
 parser.add_argument(
     "-f",
     "--file",
@@ -24,10 +39,9 @@ parser.add_argument(
 args = parser.parse_args()
 if len(sys.argv) == 1:
     exit(parser.print_help())
-print(args)
 
 if __name__ == "__main__":
-    print(f"Calling {_this_script_path}")
+    print(args.batch_file)
     print()
 
 
