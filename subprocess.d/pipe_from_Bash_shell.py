8aea30095dcba8588eba713a5d7ef10d4c215fa0
aroldo <asouzaleite@gmx.de>
2026-03-02 15:18:31 +0100

    Beautifying code.

--- subprocess.d/pipe_from_Bash_shell.py
+++ subprocess.d/pipe_from_Bash_shell.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 """
 Beispiele subprocess PIPE.
+Using a Popen object as a context manager.
+Streaming the subprocess standard output and
+the subprocess error output separately from
+each other.
 """
 import os, sys, subprocess
 from pathlib import Path
@@ -10,12 +14,13 @@ print(f"BEGIN of {this_file}")
 proc_env = dict(GARBAGE="garbage",
                 # RUBBISH="rubbish",
                 )
+# The subprocess will be executed in an extended environment:
 os.environ.update(proc_env)
 cmd_tuple = ["./pipe-to-Python.sh"]
-with subprocess.Popen(cmd_tuple,
-                       text=True,
-                       stdout = subprocess.PIPE,
-                       stderr = subprocess.PIPE) as proc:
+with subprocess.Popen(cmd_tuple, # the executable and parameters
+                      text=True,
+                      stdout = subprocess.PIPE,
+                      stderr = subprocess.PIPE) as proc:
     while True:
         out=(proc.stdout.readline()).rstrip()
         err=(proc.stderr.readline()).rstrip()
@@ -25,6 +30,7 @@ with subprocess.Popen(cmd_tuple,
             print("OUTPUT:" + out, file = sys.stdout)
         if not err == "":
             print("ERROR:" + err, file=sys.stderr)
+        # controlling the process depending on its latest output line:
         if "bbbb" in out:
             print("bbbb in out: killing the process.")
             proc.kill()
