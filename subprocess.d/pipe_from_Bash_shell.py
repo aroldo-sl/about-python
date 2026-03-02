e124d580a04bc75d1386f84f06a820807bc9a26e
aroldo <asouzaleite@gmx.de>
2026-03-02 15:13:02 +0100

    A simpler example.

---
+++ subprocess.d/pipe_from_Bash_shell.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+"""
+Beispiele subprocess PIPE.
+"""
+import os, sys, subprocess
+from pathlib import Path
+this_file = Path(sys.argv[0])
+print(f"BEGIN of {this_file}")
+
+proc_env = dict(GARBAGE="garbage",
+                # RUBBISH="rubbish",
+                )
+os.environ.update(proc_env)
+cmd_tuple = ["./pipe-to-Python.sh"]
+with subprocess.Popen(cmd_tuple,
+                       text=True,
+                       stdout = subprocess.PIPE,
+                       stderr = subprocess.PIPE) as proc:
+    while True:
+        out=(proc.stdout.readline()).rstrip()
+        err=(proc.stderr.readline()).rstrip()
+        if  (out,err) == ("",""):
+              break
+        if not out == "":
+            print("OUTPUT:" + out, file = sys.stdout)
+        if not err == "":
+            print("ERROR:" + err, file=sys.stderr)
+        if "bbbb" in out:
+            print("bbbb in out: killing the process.")
+            proc.kill()
+                  
+print(f"END of {this_file}")
+    
+
+
+
+
+
+
